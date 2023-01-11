928906dceba2618ad554981765588e45939b6b25
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2023-01-11 17:19:18 +0200

    sec

--- src/App.js
+++ src/App.js
@@ -19,7 +19,13 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
+        <div className="d-flex align-center  justify-between mb-40">
         <h1> Все кроссовки </h1>
+        <div className="search-block ">
+          <img className="mr-10" width={14} height={14} src='/img/search.png' alt='Search' />
+          <input placeholder='Поиск...' />
+        </div>
+    </div>
 
         <div className="card-wrap d-flex flex-wrap">
         <div className="card">
